41f2d3a44d59a0de906b1f7f6cce7ebd78014352
mlbridges <molly.bridges98@gmail.com>
2022-03-16 15:08:21 -0400

    added cheese louise build (with good menu screens)

--- CheeseStealthAdventure/Assets/_Scripts/EnemyControl.cs
+++ CheeseStealthAdventure/Assets/_Scripts/EnemyControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
 public class EnemyControl : MonoBehaviour
 {
